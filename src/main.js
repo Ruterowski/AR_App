adf929ed1fceff1d6d1283c1473f77b0fc3f44d8
Adrian Rutkowski <534104@student.saxion.nl>
2025-03-30 22:13:17 +0200

    Fixed the QR code detection bug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let welcomePrompt = document.getElementById("welcomePrompt");
     let sceneContainer = document.getElementById("sceneContainer");
+    let textContainer = document.getElementById("textContainer");
 
-    welcomePrompt.onclick = function () {
+    document.getElementById("firstModelButton").onclick = function () {
         enableAr();
     };
 
@@ -34,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function setupAR() {
-        // let arrow = document.getElementById("arrow");
+
         const pieces = ["first-marker", "second-marker"];
 
         for (let i = 0; i < pieces.length - 1; i++) {
@@ -43,77 +44,32 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    function setupMarkers(firstMarkerId, secondMarkerId) {
-        let secondMarkerDetected = false;
+    function setupMarkers(firstPieceId, secondPieceId, solvedPiecesId){
+        let firstPiece = document.getElementById(firstPieceId);
+        let secondPiece = document.getElementById(secondPieceId);
+        let firstPieceFound = false;
+        let stepSolved = false;
 
-        let firstMarker = document.getElementById(firstMarkerId);
-        let secondMarker = document.getElementById(secondMarkerId);
+        textContainer.style.display = "block";
+        textContainer.innerText = "Please scan the QR code of the first piece";
 
-        let text = document.getElementById("textContainer");
-        text.style.display = "block";
-        if (!firstMarker || !secondMarker) {
-            console.error(`Markers not found: ${firstMarkerId}, ${secondMarkerId}`);
-            return;
-        }
+            firstPiece.addEventListener("markerFound", function () {
+                firstPieceFound = true;
+                textContainer.innerText = "Please scan the QR code of the second piece";
+            });
 
-        if (!secondMarkerDetected) {
-            console.log("Please scan the QR code of the first piece");
-            text.innerText = "Please scan the QR code of the first piece";
-        }
+            firstPiece.addEventListener("markerLost", function () {
+                firstPieceFound = false;
+                textContainer.innerText = "Please scan the QR code of the first piece";
+            });
 
-        firstMarker.addEventListener("markerFound", function () {
-            text.innerText = "Please scan the QR code of the next piece";
-        });
-
-        secondMarker.addEventListener("markerFound", function () {
-            secondMarkerDetected = true;
-            // updateArrow(firstMarker, secondMarker);
-        });
-
-
-        secondMarker.addEventListener("markerLost", function (){
-            if(secondMarkerDetected) {
-                console.log("You have connected the pieces incorrectly");
-                text.innerText = "You have connected the pieces incorrectly";
-            }
-            secondMarkerDetected = false;
-        });
+            secondPiece.addEventListener("markerFound", function () {
+                if (firstPieceFound) {
+                    textContainer.innerText = "Both pieces detected, please follow the instructions";
+                }
+                if (!firstPieceFound) {
+                    textContainer.innerText = "You have scanned the wrong piece";
+                }
+            });
     }
-
-    function getMarkerPosition(marker) {
-        return marker.object3D.position;
-    }
-
-    // function updateArrow(firstMarker, secondMarker) {
-    //     let arrow = document.getElementById("arrow");
-    //
-    //     const marker1Pos = getMarkerPosition(firstMarker);
-    //     const marker2Pos = getMarkerPosition(secondMarker);
-    //
-    //     const dx = marker2Pos.x - marker1Pos.x;
-    //     const dz = marker2Pos.z - marker1Pos.z;
-    //
-    //     arrow.setAttribute("position", {
-    //         x: (marker1Pos.x + marker2Pos.x) / 2,
-    //         y: (marker1Pos.y + marker2Pos.y) / 2,
-    //         z: (marker1Pos.z + marker2Pos.z) / 2,
-    //     });
-    //
-    //     arrow.setAttribute("rotation", {
-    //         x: 0,
-    //         y: Math.atan2(dz, dx) * (180 / Math.PI),
-    //         z: 0,
-    //     });
-    //
-    //     const distance = Math.sqrt(dx * dx + dz * dz);
-    //     arrow.setAttribute("scale", { x: distance, y: 1, z: 1 });
-    //     arrow.setAttribute("visible", true);
-    // }
-    //
-    // function removeArrow() {
-    //     let arrow = document.getElementById("arrow");
-    //     if (arrow) {
-    //         arrow.setAttribute("visible", false);
-    //     }
-    // }
 });
